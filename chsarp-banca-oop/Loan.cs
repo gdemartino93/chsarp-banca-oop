7098c0301f41664f3154ca61ad54d43f8c97c1dc
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-02 11:54:35 +0200

    created class loan with constructor

---
+++ chsarp-banca-oop/Loan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chsarp_banca_oop
+{
+    internal class Loan
+    {
+        private int id;
+        private Customer customer;
+        private int totalLoan;
+        private int installment;
+
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public int TotalLoan { get; set; }
+        public int Installment { get; set; }
+
+        public Loan(int id, Customer customer, int totalLoan, int installment)
+        {
+            Id = id;
+            Customer = customer;
+            TotalLoan = totalLoan;
+            Installment = installment;
+        }
+    }
+}

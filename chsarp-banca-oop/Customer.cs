015f4f27156e64811ad7c8ad61ae2e3411601233
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-02 11:08:02 +0200

    created customer class with constructor

---
+++ chsarp-banca-oop/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chsarp_banca_oop
+{
+    internal class Customer
+    {
+        private string name;
+        private string lastname;
+        private string fiscalCode;
+        private int salary;
+
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string FiscalCode { get; set; }
+        public int Salary { get; set; }
+
+        public Customer(string name, string lastname,string fiscalCode, int salary)
+        {
+            Name = name;
+            Lastname = lastname;
+            FiscalCode = fiscalCode;
+            Salary = salary;
+        }
+    }
+}

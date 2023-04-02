baa271577e900fef8c170c37153c35246de1add3
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-02 11:57:13 +0200

    fix loan class

--- chsarp-banca-oop/Loan.cs
+++ chsarp-banca-oop/Loan.cs
@@ -12,18 +12,24 @@ namespace chsarp_banca_oop
         private Customer customer;
         private int totalLoan;
         private int installment;
+        private DateTime startDate;
+        private DateTime endDate;
 
         public int Id { get; set; }
         public Customer Customer { get; set; }
         public int TotalLoan { get; set; }
         public int Installment { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
-        public Loan(int id, Customer customer, int totalLoan, int installment)
+        public Loan(int id, Customer customer, int totalLoan, int installment, DateTime startDate, DateTime endDate)
         {
             Id = id;
             Customer = customer;
             TotalLoan = totalLoan;
             Installment = installment;
+            StartDate = startDate;
+            EndDate = endDate;
         }
     }
 }

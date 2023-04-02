2a802a1054d871ef6845b4a9421aa15586d4ae16
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-02 10:54:24 +0200

    Aggiungere i file di progetto.

---
+++ chsarp-banca-oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chsarp_banca_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

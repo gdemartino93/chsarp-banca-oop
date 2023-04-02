d8282edfdf88d2ebb584f53c8a94dc9b5f991696
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-02 18:38:07 +0200

    fix menu

--- chsarp-banca-oop/Program.cs
+++ chsarp-banca-oop/Program.cs
@@ -21,7 +21,7 @@ namespace chsarp_banca_oop
             Console.WriteLine("[2] Modifica Cliente");
             Console.WriteLine("[3] Ricerca Cliente");
             Console.WriteLine("[4] Ricerca prestiti di un Cliente");
-            Console.WriteLine("[5] Aggiungi prestito");
+            Console.WriteLine("[5] Ottieni il totale in euro dei prestiti per un Cliente");
             Console.WriteLine("[6] Prospetto prestiti bamca");
             Console.WriteLine("[7] Prospetto prestiti");
             Console.WriteLine("[8] Esci");

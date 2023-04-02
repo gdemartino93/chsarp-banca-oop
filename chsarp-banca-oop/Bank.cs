ec29c2c632780cbb930573f8a97e64df9d6897ff
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-02 10:56:25 +0200

    create class bank

---
+++ chsarp-banca-oop/Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chsarp_banca_oop
+{
+    internal class Bank
+    {
+        private string name;
+        private List<Cliente> customer;
+        private List<Prestito> loan;
+        public string Name { get; set; }
+    }
+}
